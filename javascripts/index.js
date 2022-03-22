8595523d7815bcfc4618189181ed31cc19cd19e0
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-22 10:41:28 -0300

    Add default theme

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,15 +1,28 @@
+/* GLOBAL VARIABLES */
+const btnChangeTheme = document.querySelector("#theme_range");
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+
+const DefaultTheme = () => {
+  body.classList.add("theme1_bg_body");
+  container.classList.add("theme1");
+};
+
 const ChangeTheme = () => {
-  const btnChangeTheme = document.querySelector("#theme_range");
-
   btnChangeTheme.addEventListener("change", () => {
-    const body = document.querySelector("body");
-    const containerBody = document.querySelector(".container");
-
-    if (btnChangeTheme.value === "1") {
-      body.classList.toggle("theme1_bg_body");
-      containerBody.classList.toggle("theme1");
+    if (btnChangeTheme.value == 1) {
+      body.classList.remove("theme1_bg_body");
+      container.classList.remove("theme1");
+      container.classList.toggle("theme2");
+      body.classList.toggle("theme2_bg_body");
+    } else if (btnChangeTheme.value == 2) {
+      body.classList.remove("theme2_bg_body");
+      container.classList.remove("theme2");
+      container.classList.add("theme1");
+      body.classList.add("theme1_bg_body");
     }
   });
 };
 
+DefaultTheme();
 ChangeTheme();
