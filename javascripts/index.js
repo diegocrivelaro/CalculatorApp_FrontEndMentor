64e886b8b2d58e0e40f4c5fffaa65871e4cd5ea3
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-22 09:47:19 -0300

    Update the structure

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,24 +3,11 @@ const ChangeTheme = () => {
 
   btnChangeTheme.addEventListener("change", () => {
     const body = document.querySelector("body");
-    const title = document.querySelector(".title");
-    const rangeDescription = document.querySelector(".range_description");
-    const changeThemeLabel = document.querySelector(".change_theme_label");
-    const themeRange = document.querySelector("#theme_range");
-    const display = document.querySelector(".display");
-    const result = document.querySelector(".result");
+    const containerBody = document.querySelector(".container");
 
     if (btnChangeTheme.value === "1") {
-      body.classList.add("theme1_bg_body");
-      title.classList.add("theme1_text_white");
-      rangeDescription.classList.add("theme1_text_white");
-      changeThemeLabel.classList.add("theme1_text_white");
-      themeRange.classList.add(
-        "theme1_style_range_srt",
-        "theme1_style_range_st"
-      );
-      display.classList.add("theme1_display_screen");
-      result.classList.add("theme1_result");
+      body.classList.toggle("theme1_bg_body");
+      containerBody.classList.toggle("theme1");
     }
   });
 };
