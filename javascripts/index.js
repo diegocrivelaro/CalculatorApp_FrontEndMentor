6cfd9ed87b78287f80e5f01aef1cbde88b5b2ca0
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-26 17:28:47 -0300

    Add theme3

--- javascripts/index.js
+++ javascripts/index.js
@@ -11,15 +11,32 @@ const DefaultTheme = () => {
 const ChangeTheme = () => {
   btnChangeTheme.addEventListener("change", () => {
     if (btnChangeTheme.value == 1) {
-      body.classList.remove("theme1_bg_body");
-      container.classList.remove("theme1");
-      container.classList.toggle("theme2");
-      body.classList.toggle("theme2_bg_body");
-    } else if (btnChangeTheme.value == 2) {
+      /* REMOVE */
       body.classList.remove("theme2_bg_body");
       container.classList.remove("theme2");
+      body.classList.remove("theme3_bg_body");
+      container.classList.remove("theme3");
+      /*------*/
+      container.classList.add("theme2");
+      body.classList.add("theme2_bg_body");
+    } else if (btnChangeTheme.value == 2) {
+      /* REMOVE */
+      body.classList.remove("theme1_bg_body");
+      container.classList.remove("theme1");
+      body.classList.remove("theme3_bg_body");
+      container.classList.remove("theme3");
+      /*------*/
       container.classList.add("theme1");
       body.classList.add("theme1_bg_body");
+    } else if (btnChangeTheme.value == 3) {
+      /* REMOVE */
+      body.classList.remove("theme1_bg_body");
+      container.classList.remove("theme1");
+      body.classList.remove("theme2_bg_body");
+      container.classList.remove("theme2");
+      /*------*/
+      container.classList.add("theme3");
+      body.classList.add("theme3_bg_body");
     }
   });
 };
