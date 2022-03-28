cab721893935f0f6e84ba8777727d0254d572ae6
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-28 14:38:45 -0300

    Refactor JavaScript Code

--- javascripts/index.js
+++ javascripts/index.js
@@ -11,30 +11,33 @@ const DefaultTheme = () => {
 const ChangeTheme = () => {
   btnChangeTheme.addEventListener("change", () => {
     if (btnChangeTheme.value == 1) {
-      /* REMOVE */
-      body.classList.remove("theme2_bg_body");
-      container.classList.remove("theme2");
-      body.classList.remove("theme3_bg_body");
-      container.classList.remove("theme3");
-      /*------*/
-      container.classList.add("theme2");
-      body.classList.add("theme2_bg_body");
-    } else if (btnChangeTheme.value == 2) {
-      /* REMOVE */
-      body.classList.remove("theme1_bg_body");
-      container.classList.remove("theme1");
-      body.classList.remove("theme3_bg_body");
-      container.classList.remove("theme3");
-      /*------*/
+      if (body.classList.contains("theme2_bg_body")) {
+        body.classList.remove("theme2_bg_body");
+        container.classList.remove("theme2");
+      } else if (body.classList.contains("theme3_bg_body")) {
+        body.classList.remove("theme3_bg_body");
+        container.classList.remove("theme3");
+      }
       container.classList.add("theme1");
       body.classList.add("theme1_bg_body");
+    } else if (btnChangeTheme.value == 2) {
+      if (body.classList.contains("theme1_bg_body")) {
+        body.classList.remove("theme1_bg_body");
+        container.classList.remove("theme1");
+      } else if (body.classList.contains("theme3_bg_body")) {
+        body.classList.remove("theme3_bg_body");
+        container.classList.remove("theme3");
+      }
+      container.classList.add("theme2");
+      body.classList.add("theme2_bg_body");
     } else if (btnChangeTheme.value == 3) {
-      /* REMOVE */
-      body.classList.remove("theme1_bg_body");
-      container.classList.remove("theme1");
-      body.classList.remove("theme2_bg_body");
-      container.classList.remove("theme2");
-      /*------*/
+      if (body.classList.contains("theme1_bg_body")) {
+        body.classList.remove("theme1_bg_body");
+        container.classList.remove("theme1");
+      } else if (body.classList.contains("theme2_bg_body")) {
+        body.classList.remove("theme2_bg_body");
+        container.classList.remove("theme2");
+      }
       container.classList.add("theme3");
       body.classList.add("theme3_bg_body");
     }
